816f411518e3314c1b92346efe92fbd5035bcb49
Max Heidinger <mail@max-heidinger.de>
2018-06-05 08:35:00 +0200

    Remove xml file => Create xml data from js object

--- server.js
+++ server.js
@@ -1,17 +1,41 @@
 const express = require("express");
 const path = require("path");
+const convert = require('xml-js');
 
 const app = express();
 
-const fileOptions = {
-  root: __dirname + "/client/",
-  dotfiles: "ignore",
+var xmlOptions = {compact: true, ignoreComment: true, spaces: 2};
+
+const catalog = {
+  _declaration: { _attributes: {version: "1.0", encoding: "UTF-8" } },
+  _instruction: { "xml-stylesheet": 'type="text/xsl" href="xsl/index.xsl"' },
+  catalog: {
+    cd: [
+      {
+        title: "Empire Birlesque",
+        artist: "Bob Dylan",
+        country: "USA",
+        company: "Columbia",
+        price: 10.90,
+        year: 1985
+      },
+      {
+        title: "asdsad sadsadsad",
+        artist: "Bob Dylan",
+        country: "USA",
+        company: "Columbia",
+        price: 10.90,
+        year: 1985
+      }
+    ]
+  }
 }
 
 app.get("/", (req, res) => {
-  res.sendFile("index.xml", fileOptions);
+  res.setHeader("Content-Type", "text/xml")
+  res.send(convert.json2xml(catalog, xmlOptions))
 });
 
-app.use("/xsl", express.static(path.join(__dirname, "client", "xsl")));
+app.use("/xsl", express.static(path.join(__dirname, "client")));
 
 app.listen(9991, () => console.log("Listening on http://127.0.0.1:9991"));
