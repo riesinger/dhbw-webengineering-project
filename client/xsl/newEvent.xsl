5655ebcba3bb9b04d25992c778fb67793efa920c
riesinger <riesinger.pascal@gmail.com>
2018-07-02 09:02:12 +0200

    Fix day on day view, move xsl files into own folder

---
+++ client/xsl/newEvent.xsl
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform" xmlns="http://www.w3.org/1999/xhtml">
+    <xsl:output method="xml" doctype-public="-//W3C//DTD XHTML 1.1//EN" doctype-system="http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd" encoding="UTF-8"/>
+    <xsl:template match="newEventWindow">
+        <div id="newEventWindowDiv" class="card">
+            <h3 class="centeredText">Neuer Termin</h3>
+            <div>
+                <form id="newEventForm" method="post">
+					<xsl:attribute name="action">/addEvent?<xsl:value-of select="//meta/dispForm"/>=<xsl:value-of select="//meta/dateOffset"/></xsl:attribute>
+                    <div class="newEventFormDiv">
+                        Termin Name:<br/>
+                        <input type="text" name="eventName" style="width: 80%; margin-left: 0px;" required="required"/> <br/>
+                    </div>
+                    <div class="newEventFormDiv">
+                        <table>
+                            <tr>
+                                <td>Von</td>
+                                <td><input type="date" name="eventStartDate" required="required">
+                                    <xsl:attribute name="value"><xsl:value-of select="//week/currentDate/@year"/>-<xsl:if test="//week/currentDate/@month &gt; 9"><xsl:value-of select="//week/currentDate/@month"/></xsl:if><xsl:if test="//week/currentDate/@month &lt; 10">0<xsl:value-of select="//week/currentDate/@month"/></xsl:if>-<xsl:if test="//week/currentDate/@day &gt; 9"><xsl:value-of select="//week/currentDate/@day"/></xsl:if><xsl:if test="//week/currentDate/@day &lt; 10">0<xsl:value-of select="//week/currentDate/@day"/></xsl:if></xsl:attribute>
+                                </input></td>
+                                <td> um </td>
+                                <td>
+                                    <input type="time" name="eventStartTime" required="required">
+                                        <xsl:attribute name="value"><xsl:if test="//week/currentDate/@hour &lt; 10">0<xsl:value-of
+                                                select="//week/currentDate/@hour"/></xsl:if><xsl:if test="//week/currentDate/@hour &gt; 9"><xsl:value-of
+                                                select="//week/currentDate/@hour"/></xsl:if>:<xsl:if test="//week/currentDate/@minute &lt; 10">0<xsl:value-of
+                                                select="//week/currentDate/@minute"/></xsl:if><xsl:if test="//week/currentDate/@minute &gt; 9"><xsl:value-of
+                                                select="//week/currentDate/@minute"/></xsl:if></xsl:attribute>
+                                    </input>
+                                </td>
+                                <td>Uhr</td>
+                            </tr>
+                            <tr>
+                                <td>Bis</td>
+                                <td><input type="date" name="eventEndDate" required="required">
+                                    <xsl:attribute name="value"><xsl:value-of select="//week/currentDate/@year"/>-<xsl:if test="//week/currentDate/@month &gt; 9"><xsl:value-of select="//week/currentDate/@month"/></xsl:if><xsl:if test="//week/currentDate/@month &lt; 10">0<xsl:value-of select="//week/currentDate/@month"/></xsl:if>-<xsl:if test="//week/currentDate/@day &gt; 9"><xsl:value-of select="//week/currentDate/@day"/></xsl:if><xsl:if test="//week/currentDate/@day &lt; 10">0<xsl:value-of select="//week/currentDate/@day"/></xsl:if></xsl:attribute>
+                                </input></td>
+                                <td> um </td>
+                                <td>
+                                    <input type="time" name="eventEndTime" required="required"/>
+                                </td>
+                                <td>Uhr</td>
+                            </tr>
+                        </table>
+                    </div>
+                    <hr style="width:90%; opacity:0.5;"/>
+                    <div class="newEventFormDiv">
+                        Ort:<br/>
+                        <input type="text" name="eventLocation" style="width: 80%; margin-left: 0px;"/> <br/>
+                        Details:<br/>
+                        <textarea form="newEventForm" style="margin-top: 5px; width: 80%; resize: none" name="eventDescription" rows="5"/>
+                        <br/>
+                    </div>
+                    <div class="newEventButtonsDiv">
+                        <input type="submit" value="Zum Kalender hinzufügen"/>
+                    </div>
+                </form>
+            </div>
+            <a>
+                <xsl:attribute name="href">/?<xsl:value-of select="//meta/dispForm"/>=<xsl:value-of select="//meta/dateOffset"/></xsl:attribute>
+                <button class="closePopupButton">x</button>
+            </a>
+        </div>
+    </xsl:template>
+</xsl:stylesheet>
--- router.js
+++ router.js
@@ -11,264 +11,251 @@ const calendar = require("./calendar");
 const app = express();
 
 function injectXSLT(xml, xslt) {
-  let slimXML = "";
-  if (xml.indexOf("<?xml version") >= 0) {
-    slimXML = xml.substring(xml.indexOf("\n") + 1);
-  } else {
-    slimXML = xml;
-  }
-  return (
-    '<?xml version="1.0" encoding="UTF-8" ?>\n<?xml-stylesheet type="text/xsl" href="xsl/' +
-    xslt +
-    '" ?>\n' +
-    slimXML
-  );
+	let slimXML = "";
+	if (xml.indexOf("<?xml version") >= 0) {
+		slimXML = xml.substring(xml.indexOf("\n") + 1);
+	} else {
+		slimXML = xml;
+	}
+	return (
+		'<?xml version="1.0" encoding="UTF-8" ?>\n<?xml-stylesheet type="text/xsl" href="xsl/' +
+		xslt +
+		'" ?>\n' +
+		slimXML
+	);
 }
 
 function sendCalendar(res, events, dispForm, injectTags) {
-  res.setHeader("Content-Type", "text/xml");
-  try {
-		let sendObject = { calendar: { meta: injectTags } };
+	res.setHeader("Content-Type", "text/xml");
+	try {
+		let sendObject = {calendar: {meta: injectTags}};
 		sendObject.calendar[dispForm] = events;
-    const s = xmlBuilder.buildObject(sendObject);
-    res.send(injectXSLT(s, "index.xsl"));
-  } catch (err) {
-    console.error(err);
-    res.sendStatus(500);
-  }
+		const s = xmlBuilder.buildObject(sendObject);
+		res.send(injectXSLT(s, "index.xsl"));
+	} catch (err) {
+		console.error(err);
+		res.sendStatus(500);
+	}
 }
 
 function getSelectedDate(req) {
-  if (req.query.week !== undefined) {
-    return { dispForm: "week", dateOffset: parseInt(req.query.week) };
-  } else if (req.query.day !== undefined) {
-    return { dispForm: "day", dateOffset: parseInt(req.query.day) };
-  } else {
-    return { dispForm: "week", dateOffset: 0 };
-  }
+	if (req.query.week !== undefined) {
+		return {dispForm: "week", dateOffset: parseInt(req.query.week)};
+	} else if (req.query.day !== undefined) {
+		return {dispForm: "day", dateOffset: parseInt(req.query.day)};
+	} else {
+		return {dispForm: "week", dateOffset: 0};
+	}
 }
 
-exports.setup = function() {
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
-  app.use(cookieParser());
+exports.setup = function () {
+	app.use(bodyParser.urlencoded({extended: false}));
+	app.use(bodyParser.json());
+	app.use(cookieParser());
 
-  app.use((req, res, next) => {
-    if (req.path === "/login" || req.path.indexOf("css") > -1 || req.path.indexOf("dtd") > -1) return next();
+	app.use((req, res, next) => {
+		if (req.path === "/login" || req.path.indexOf("css") > -1 || req.path.indexOf("dtd") > -1) return next();
 
-    let cookie = req.cookies.token;
-    if (cookie) {
-      let splitToken = cookie.split(":");
+		let cookie = req.cookies.token;
+		if (cookie) {
+			let splitToken = cookie.split(":");
 
-      if (!users.checkToken(splitToken[0], splitToken[1])) {
-        res.cookie("token", "");
-        res.redirect("/login");
-        return;
-      }
+			if (!users.checkToken(splitToken[0], splitToken[1])) {
+				res.cookie("token", "");
+				res.redirect("/login");
+				return;
+			}
 
-      req.user = splitToken[0];
-      next();
-    } else {
-      res.redirect("/login");
-    }
-  });
+			req.user = splitToken[0];
+			next();
+		} else {
+			res.redirect("/login");
+		}
+	});
 
-  app.use(
-    "/xsl",
-    express.static(path.join(__dirname, "client"), {
-      setHeaders: (res, path) => {
-        if (path.indexOf(".xsl") > -1) {
-          res.setHeader("Content-Type", "text/xsl");
-        }
-      }
-    })
-  );
+	app.use("/xsl", express.static(path.join(__dirname, "client", "xsl"), {
+			setHeaders: (res, path) => {
+				if (path.indexOf(".xsl") > -1) {
+					res.setHeader("Content-Type", "text/xsl");
+				}
+			}
+		})
+	);
 
-  app.use("/css", express.static(path.join(__dirname, "client", "css")));
-  app.use("/images", express.static(path.join(__dirname, "client", "images")));
-  app.use("/dtd", express.static(path.join(__dirname, "dtd")));
+	app.use("/css", express.static(path.join(__dirname, "client", "css")));
+	app.use("/images", express.static(path.join(__dirname, "client", "images")));
+	app.use("/dtd", express.static(path.join(__dirname, "dtd")));
 
-  app.get("/", async (req, res) => {
-    console.log("Getting calendar for user", req.user);
-    try {
-      const selectedDate = getSelectedDate(req);
-      const oEvents = await calendar.getEvents(req.user, selectedDate);
-      sendCalendar(res, oEvents, selectedDate.dispForm, { ...selectedDate });
-    } catch (err) {
-      console.error(err);
-      res.statusCode(500);
-    }
-  });
+	app.get("/", async (req, res) => {
+		console.log("Getting calendar for user", req.user);
+		try {
+			const selectedDate = getSelectedDate(req);
+			const oEvents = await calendar.getEvents(req.user, selectedDate);
+			sendCalendar(res, oEvents, selectedDate.dispForm, {...selectedDate});
+		} catch (err) {
+			console.error(err);
+			res.statusCode(500);
+		}
+	});
 
-  app.get("/login", async (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "login.html"));
-  });
+	app.get("/login", async (req, res) => {
+		res.sendFile(path.join(__dirname, "client", "login.html"));
+	});
 
-  app.post("/login", async (req, res) => {
-    let token = users.checkCredentials(req.body.username, req.body.password);
-    if (token === "") {
-      console.error("Wrong credentials!");
-      res.cookie("token", "");
-      res.redirect("/login");
-    } else {
-      console.log("Successfull authentication: Set cookie and redirect to /");
-      res.cookie("token", token, {
-        expires: new Date(253402300000000),
-        httpOnly: true
-      });
-      res.redirect("/");
-    }
-  });
+	app.post("/login", async (req, res) => {
+		let token = users.checkCredentials(req.body.username, req.body.password);
+		if (token === "") {
+			console.error("Wrong credentials!");
+			res.cookie("token", "");
+			res.redirect("/login");
+		} else {
+			console.log("Successfull authentication: Set cookie and redirect to /");
+			res.cookie("token", token, {
+				expires: new Date(253402300000000),
+				httpOnly: true
+			});
+			res.redirect("/");
+		}
+	});
 
-  app.post("/addEvent", (req, res) => {
-    const startDate = req.body.eventStartDate.split("-");
-    const endDate = req.body.eventEndDate.split("-");
-    const startTime = req.body.eventStartTime.split(":");
-	  const endTime = req.body.eventEndTime.split(":");
+	app.post("/addEvent", (req, res) => {
+		const startDate = req.body.eventStartDate.split("-");
+		const endDate = req.body.eventEndDate.split("-");
+		const startTime = req.body.eventStartTime.split(":");
+		const endTime = req.body.eventEndTime.split(":");
 
-	  let loc = req.body.eventLocation;
-	  if(loc == null) loc = "";
+		let loc = req.body.eventLocation;
+		if (loc == null) loc = "";
 
-	  let desc = req.body.eventDescription;
-	  if(desc == null) desc = "";
+		let desc = req.body.eventDescription;
+		if (desc == null) desc = "";
 
-    calendar
-      .addEventToCalendar(req.user, {
-        name: req.body.eventName,
-        description: desc,
-        location: loc,
-        startDateDay: Number(startDate[2]),
-        startDateMonth: Number(startDate[1]),
-        startDateYear: Number(startDate[0]),
-        startTimeHour: Number(startTime[0]),
-        startTimeMinute: Number(startTime[1]),
-        endDateDay: Number(endDate[2]),
-        endDateMonth: Number(endDate[1]),
-        endDateYear: Number(endDate[0]),
-        endTimeHour: Number(endTime[0]),
-        endTimeMinute: Number(endTime[1])
-      })
-      .then(
-        res => {
-          console.log("Event successfully added!");
-        },
-        err => {
-          console.error(err);
-        }
-      );
+		calendar.addEventToCalendar(req.user, {
+			name: req.body.eventName,
+			description: desc,
+			location: loc,
+			startDateDay: Number(startDate[2]),
+			startDateMonth: Number(startDate[1]),
+			startDateYear: Number(startDate[0]),
+			startTimeHour: Number(startTime[0]),
+			startTimeMinute: Number(startTime[1]),
+			endDateDay: Number(endDate[2]),
+			endDateMonth: Number(endDate[1]),
+			endDateYear: Number(endDate[0]),
+			endTimeHour: Number(endTime[0]),
+			endTimeMinute: Number(endTime[1])
+		})
+			.then(() => console.log("Event successfully added!"))
+			.catch(console.error);
 
 		const date = getSelectedDate(req);
-    res.redirect("/?" + date.dispForm + "=" + date.dateOffset);
-  });
+		res.redirect("/?" + date.dispForm + "=" + date.dateOffset);
+	});
 
-  app.post("/editEvent",async (req,res) => {
-    console.log("Getting change on calender for user " + req.user);
-        let eventID = req.query.eventID;
-        console.log("Event ID: "  + eventID);
-        if (eventID) {
-            await calendar.removeEventFromCalendar(req.user, eventID);
-            console.log("Reomved event from calender")
+	app.post("/editEvent", async (req, res) => {
+		console.log("Getting change on calender for user " + req.user);
+		let eventID = req.query.eventID;
+		if (eventID) {
+			await calendar.removeEventFromCalendar(req.user, eventID);
 
-            var startDate = req.body.eventStartDate.split('-');
-            var endDate = req.body.eventEndDate.split('-');
-            var startTime = req.body.eventStartTime.split(":");
-            var endTime = req.body.eventEndTime.split(":");
+			const startDate = req.body.eventStartDate.split('-');
+			const endDate = req.body.eventEndDate.split('-');
+			const startTime = req.body.eventStartTime.split(":");
+			const endTime = req.body.eventEndTime.split(":");
 
-            var loc = req.body.eventLocation;
-            if(loc == null) loc = "";
+			let loc = req.body.eventLocation;
+			if (loc == null) loc = "";
 
-            var desc = req.body.eventDescription;
-            if(desc == null) desc = "";
+			let desc = req.body.eventDescription;
+			if (desc == null) desc = "";
 
-            await calendar.addEventToCalendar(req.user, {
-                name: req.body.eventName,
-                description: desc,
-                location: loc,
-                startDateDay: Number(startDate[2]),
-                startDateMonth: Number(startDate[1]),
-                startDateYear: Number(startDate[0]),
-                startTimeHour: Number(startTime[0]),
-                startTimeMinute: Number(startTime[1]),
-                endDateDay: Number(endDate[2]),
-                endDateMonth: Number(endDate[1]),
-                endDateYear: Number(endDate[0]),
-                endTimeHour: Number(endTime[0]),
-                endTimeMinute: Number(endTime[1])
-            }).then((res) => {
-                console.log("Changed event successfully!");
-            }, (err) => {
-                console.error(err);
-            });
-        }
-        
-      const date = getSelectedDate(req);
-      res.redirect("/?" + date.dispForm + "=" + date.dateOffset);
+			await calendar.addEventToCalendar(req.user, {
+				name: req.body.eventName,
+				description: desc,
+				location: loc,
+				startDateDay: Number(startDate[2]),
+				startDateMonth: Number(startDate[1]),
+				startDateYear: Number(startDate[0]),
+				startTimeHour: Number(startTime[0]),
+				startTimeMinute: Number(startTime[1]),
+				endDateDay: Number(endDate[2]),
+				endDateMonth: Number(endDate[1]),
+				endDateYear: Number(endDate[0]),
+				endTimeHour: Number(endTime[0]),
+				endTimeMinute: Number(endTime[1])
+			}).then(() => {
+				console.log("Changed event successfully!");
+			}).catch(console.error);
+		}
+
+		const date = getSelectedDate(req);
+		res.redirect("/?" + date.dispForm + "=" + date.dateOffset);
 
 	});
 
-  app.get("/newEvent", async (req, res) => {
-    try {
-      const selectedDate = getSelectedDate(req);
-      const oEvents = await calendar.getEvents(req.user, selectedDate);
-      sendCalendar(res, oEvents, selectedDate.dispForm, {
-        newEventWindow: {},
-        ...selectedDate
-      });
-    } catch (err) {
-      console.error(err);
-      res.statusCode(500);
-    }
-  });
+	app.get("/newEvent", async (req, res) => {
+		try {
+			const selectedDate = getSelectedDate(req);
+			const oEvents = await calendar.getEvents(req.user, selectedDate);
+			sendCalendar(res, oEvents, selectedDate.dispForm, {
+				newEventWindow: {},
+				...selectedDate
+			});
+		} catch (err) {
+			console.error(err);
+			res.statusCode(500);
+		}
+	});
 
-  app.get("/showEvent", async (req, res) => {
-    let eventID = req.query.eventID;
-    if (eventID) {
-      try {
+	app.get("/showEvent", async (req, res) => {
+		let eventID = req.query.eventID;
+		if (eventID) {
+			try {
 				const selectedDate = getSelectedDate(req);
 				const oEvents = await calendar.getEvents(req.user, selectedDate);
 				sendCalendar(res, oEvents, selectedDate.dispForm, {
-					showEvent: { ID: eventID },
+					showEvent: {ID: eventID},
 					...selectedDate
 				});
-      } catch (err) {
-        console.error(err);
-        res.statusCode(500);
-      }
-    } else {
-      res.redirect("/");
-    }
-  });
+			} catch (err) {
+				console.error(err);
+				res.statusCode(500);
+			}
+		} else {
+			res.redirect("/");
+		}
+	});
 
-  app.get("/deleteEvent", async (req, res) => {
-    let eventID = req.query.eventID;
-    if (eventID) {
-      console.log("Remove event " + eventID + " for user " + req.user);
-      await calendar.removeEventFromCalendar(req.user, eventID);
-    }
+	app.get("/deleteEvent", async (req, res) => {
+		let eventID = req.query.eventID;
+		if (eventID) {
+			console.log("Remove event " + eventID + " for user " + req.user);
+			await calendar.removeEventFromCalendar(req.user, eventID);
+		}
 
-    res.redirect("/");
-  });
+		res.redirect("/");
+	});
 
-    app.get("/editEvent", async (req, res) => {
-        try {
-            let eventID = req.query.eventID;
-            if(eventID) {
-                const selectedDate = getSelectedDate(req);
-                const oEvents = await calendar.getEvents(req.user, selectedDate);
-                sendCalendar(res, oEvents, selectedDate.dispForm, {
-                    editEvent: { ID: eventID },
-                    ...selectedDate
-                });
-            } else {
-                res.redirect("/");
-            }
-        } catch (err) {
-            console.error(err);
-            res.statusCode(500);
-        }
-    });
+	app.get("/editEvent", async (req, res) => {
+		try {
+			let eventID = req.query.eventID;
+			if (eventID) {
+				const selectedDate = getSelectedDate(req);
+				const oEvents = await calendar.getEvents(req.user, selectedDate);
+				sendCalendar(res, oEvents, selectedDate.dispForm, {
+					editEvent: {ID: eventID},
+					...selectedDate
+				});
+			} else {
+				res.redirect("/");
+			}
+		} catch (err) {
+			console.error(err);
+			res.statusCode(500);
+		}
+	});
 };
 
-exports.start = function() {
-  app.listen(9991, () => console.log("Listening on http://127.0.0.1:9991"));
+exports.start = function () {
+	app.listen(9991, () => console.log("Listening on http://127.0.0.1:9991"));
 };
